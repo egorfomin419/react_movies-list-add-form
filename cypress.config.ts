c8a76700b546bc0ead75c46560db5c89a8d1bd07
Egor Fomin <egorfomin419@gmail.com>
2026-08-15 22:28:44 +0300

    fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,23 +1,32 @@
-const { defineConfig } = require('cypress');
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/integration/**/*.spec.{js,ts,jsx,tsx}',
   },
+
   video: true,
+
   viewportHeight: 1920,
   viewportWidth: 1080,
+
   screenshotOnRunFailure: true,
+
   reporter: 'mochawesome',
+
   reporterOptions: {
     reportDir: 'raw_reports',
     overwrite: false,
     html: false,
     json: true,
   },
+
   component: {
     specPattern: 'src/**/*.spec.{js,ts,jsx,tsx}',
+
     devServer: {
       framework: 'react',
       bundler: 'vite',
